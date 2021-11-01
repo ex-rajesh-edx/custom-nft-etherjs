ee5640775ccccafcd0d7564f425aa14a7497d962
rajeshr-edx <91868710+rajeshr-edx@users.noreply.github.com>
2021-11-01 17:41:38 +0530

    api errors middleware added

--- server/src/config/express.js
+++ server/src/config/express.js
@@ -7,7 +7,7 @@ const helmet = require('helmet');
 const adminRoutes = require("../api/admin/routes/routes");
 const clientRoutes = require("../api/client/routes/routes");
 const { logs } = require('./variables');
-// const error = require('../api/middlewares/error');
+const error = require('../api/middlewares/error');
 /**
 * Error handling
 * @ToDo
@@ -46,12 +46,12 @@ app.use('/admin', adminRoutes);
 app.use('/client', clientRoutes);
 
 // if error is not an instanceOf APIError, convert it.
-// app.use(error.converter);
+app.use(error.converter);
 
 // catch 404 and forward to error handler
-// app.use(error.notFound);
+app.use(error.notFound);
 
 // error handler, send stacktrace only during development
-// app.use(error.handler);
+app.use(error.handler);
 
 module.exports = app;
